9b08f0a6b71a8c3126ef0e2ea7086e7adb3d2c36
Novák Viktor <novk.viktor@gmail.com>
2023-12-15 10:29:56 +0100

    Day15 part 2

--- src/main/kotlin/com/adventofcode/day/Day15.kt
+++ src/main/kotlin/com/adventofcode/day/Day15.kt
@@ -1,16 +1,61 @@
 package com.adventofcode.day
 
 class Day15(inputFile: String): DayXX(inputFile) {
+    private data class Lens(val label: String, var amount: Int)
+    private data class Box(val id: Int, var lens: MutableList<Lens>)
+
     override fun part1(): Int = input.first().split(",")
         .sumOf { hash(it) }
 
-    override fun part2() {
+    override fun part2(): Int {
+        val boxes = arrayListOf<Box>()
+        input.first().split(",").forEach {
+            if (it.contains("=")) {
+                val (label, amount) = it.split("=")
+                val boxNumber = hash(label) + 1
+                val box = boxes.find { it.id == boxNumber }
+                if (box != null) {
+                    val lens = box.lens.find { it.label == label }
+                    if (lens != null) {
+                        lens.amount = amount.toInt()
+                    } else {
+                        box.lens.add(Lens(label, amount.toInt()))
+                    }
+                } else {
+                    boxes.add(
+                        Box(
+                            boxNumber,
+                            arrayListOf(Lens(label, amount.toInt()))
+                        )
+                    )
+                }
+            } else {
+                val label = it.dropLast(1)
+                val boxNumber = hash(label) + 1
+                val box = boxes.find { it.id == boxNumber }
+                if (box != null) {
+                    val lens = box.lens.find { it.label == label }
+                    if (lens != null) {
+                        box.lens = box.lens.filter { it.label != label }.toMutableList()
+                    }
+                }
+            }
+        }
+
+        var result = 0
+        boxes.forEach { (id, lens) ->
+            lens.forEachIndexed { index, (_, amount) ->
+                result += id * (index + 1) * amount
+            }
+        }
+
+        return result
     }
 
     private fun hash(input: String): Int {
         var result = 0
         input.forEach {
-            result += it.toInt()
+            result += it.code
             result *= 17
             result %= 256
         }
@@ -20,5 +65,5 @@ class Day15(inputFile: String): DayXX(inputFile) {
 }
 
 fun main() {
-    Day15("day15").solve(1)
+    Day15("day15").solve()
 }
